fa5f10fc2d14a4f71e2771d7b1c617d19203ae7e
Simon Walker <s.r.walker101@googlemail.com>
2024-10-03 09:40:22 +0100

    Start rust project

--- shell.nix
+++ shell.nix
@@ -1,20 +1,40 @@
 { pkgs ? import <nixpkgs> { } }:
 with pkgs;
-mkShell {
+mkShell rec {
   packages = [
+
+
     python3
     python3Packages.venvShellHook
     uv
     hyperfine
+
+      rustup
+    clang
+  ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+    libiconv
+  ]) ++ lib.optionals stdenv.isLinux [
+    mold
   ];
 
   venvDir = ".venv";
 
-  postVenvCreation = ''
+  shellHook = ''
+    export RUST_BUILD_BASE="$HOME/.cache/rust-builds"
+    WORKSPACE_ROOT=$(cargo metadata --no-deps --offline 2>/dev/null | jq -r ".workspace_root")
+    PACKAGE_BASENAME=$(basename $WORKSPACE_ROOT)
+
+    # Run cargo with target set to $RUST_BUILD_BASE/$PACKAGE_BASENAME
+    export CARGO_TARGET_DIR="$RUST_BUILD_BASE/$PACKAGE_BASENAME"
   '';
 
   postShellHook = ''
     export VENV_DIR=$VIRTUAL_ENV
   '';
+
+  env = {
+    RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+    LD_LIBRARY_PATH = lib.makeLibraryPath packages;
+  };
 }
 
