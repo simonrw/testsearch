72256dd144513a7f415b5ba7c9d00eb801369160
Simon Walker <s.r.walker101@googlemail.com>
2024-10-17 11:52:47 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Flake utils demo";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+        ];
+
+        pkgs = import nixpkgs {
+          inherit overlays system;
+        };
+      in
+      {
+        packages.default = import ./default.nix { inherit pkgs; };
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      }
+    );
+}
