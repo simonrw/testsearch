3aab4cfa6e993196ec8eb3df102727f048708dac
Simon Walker <s.r.walker101@googlemail.com>
2024-11-11 11:00:52 +0000

    Default to rust package in nix

--- default.nix
+++ default.nix
@@ -1,23 +1,34 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs;
-python3Packages.buildPythonApplication {
-  pname = "testsearch";
-  version = "0.1.0";
+{pkgs ? import <nixpkgs> {}}:
+with pkgs; let
+  python-app = python3Packages.buildPythonApplication {
+    pname = "testsearch";
+    version = "0.1.0";
 
-  src = ./.;
+    src = ./.;
 
-  pyproject = true;
+    pyproject = true;
 
-  dependencies = with python3Packages; [
-    setuptools
-    tree-sitter
-    tree-sitter-python
-    (iterfzf.overrideAttrs (prev:  {
-      doInstallCheck = false;
-    }))
-  ];
+    dependencies = with python3Packages; [
+      setuptools
+      tree-sitter
+      tree-sitter-python
+      (iterfzf.overrideAttrs (prev: {
+        doInstallCheck = false;
+      }))
+    ];
 
-  propagatedBuildInputs = [
-    fd
-  ];
-}
+    propagatedBuildInputs = [
+      fd
+    ];
+  };
+
+  rust-app = rustPlatform.buildRustPackage {
+    pname = "testsearch";
+    version = "0.1.0";
+
+    src = ./.;
+
+    cargoLock.lockFile = ./Cargo.lock;
+  };
+in
+  rust-app
