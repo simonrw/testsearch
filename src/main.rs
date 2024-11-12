b5956759d0c3b75845df853f54238616fd46d4ad
Simon Walker <s.r.walker101@googlemail.com>
2024-11-12 17:04:48 +0000

    Restructure arguments to allow for state management commands

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::{
     thread,
 };
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use color_eyre::eyre::{self, Context};
 use ignore::WalkBuilder;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
@@ -35,15 +35,42 @@ impl FromStr for CacheClearOption {
     }
 }
 
+#[derive(Subcommand, Debug, Clone)]
+enum Command {
+    /// Search for a test or rerun the last test
+    Search {
+        /// Paths to search for tests
+        root: Vec<PathBuf>,
+
+        /// Print results rather than using fuzzy find
+        #[arg(short, long)]
+        no_fizzy_selection: bool,
+
+        /// Re-run the last test
+        #[arg(short, long)]
+        rerun_last: bool,
+    },
+    /// View or manage state
+    State {
+        #[command(subcommand)]
+        state_command: StateCommand,
+    },
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum StateCommand {
+    /// Clear the state
+    Clear {
+        /// Clear the state for all directories
+        #[arg(short, long)]
+        all: bool,
+    },
+}
+
 #[derive(Debug, Parser)]
 struct Args {
-    root: Vec<PathBuf>,
-    #[clap(short, long)]
-    no_fizzy_selection: bool,
-    #[clap(short, long)]
-    rerun_last: bool,
-    #[clap(short, long)]
-    clear_cache: Option<CacheClearOption>,
+    #[command(subcommand)]
+    command: Command,
 }
 
 fn current_dir() -> eyre::Result<PathBuf> {
@@ -162,105 +189,120 @@ fn main() -> eyre::Result<()> {
     tracing::debug!(cache_root = %cache_root.display(), "using cache root dir");
     let mut state = State::new(cache_root).wrap_err("constructing persistent state")?;
 
-    if let Some(cache_clear_option) = args.clear_cache {
-        state
-            .clear(cache_clear_option)
-            .wrap_err("clearing cache state")?;
-    }
+    match args.command {
+        Command::Search {
+            root,
+            no_fizzy_selection,
+            rerun_last,
+        } => {
+            if rerun_last {
+                match state.last_test() {
+                    Some(name) => {
+                        println!("{name}");
+                        return Ok(());
+                    }
+                    None => eyre::bail!("No last test recorded"),
+                }
+            }
 
-    if args.rerun_last {
-        match state.last_test() {
-            Some(name) => {
-                println!("{name}");
+            let (files_tx, files_rx) = unbounded();
+
+            // check that some files were passed, otherwise default to the current working directory
+            let search_roots = if root.is_empty() {
+                let here = current_dir()?;
+                vec![here]
+            } else {
+                root
+            };
+
+            let mut file_handles = Vec::new();
+            for path in search_roots {
+                let span = tracing::debug_span!("", path = %path.display());
+                let _guard = span.enter();
+
+                tracing::debug!("listing files");
+
+                let files_tx = files_tx.clone();
+                file_handles.push(thread::spawn(move || {
+                    if let Err(e) = find_test_files(&path, files_tx) {
+                        tracing::warn!(error = %e, path = %path.display(), "finding test files");
+                    }
+                }));
+            }
+            drop(files_tx);
+
+            for handle in file_handles {
+                let _ = handle.join();
+            }
+
+            let files: Vec<_> = files_rx.into_iter().collect();
+            if files.is_empty() {
+                eyre::bail!("No compatible test files found");
+            }
+
+            tracing::debug!(n = files.len(), "finished collecting files");
+
+            let (test_tx, test_rx) = unbounded();
+            files
+                .into_par_iter()
+                .for_each_with(test_tx, |sender, path| {
+                    if let Err(e) = parse_file(sender, &path) {
+                        tracing::warn!(error = %e, path = %path.display(), "error parsing file");
+                    }
+                });
+
+            if no_fizzy_selection {
+                for test in test_rx {
+                    println!("{}", test.text());
+                }
+
                 return Ok(());
             }
-            None => eyre::bail!("No last test recorded"),
-        }
-    }
 
-    let (files_tx, files_rx) = unbounded();
+            // perform fuzzy search
+            let skim_options = SkimOptionsBuilder::default()
+                .multi(false)
+                .build()
+                .expect("invalid skim options");
+            let search_result = skim::Skim::run_with(&skim_options, Some(test_rx))
+                .ok_or_else(|| eyre::eyre!("performing interactive search"))?;
 
-    // check that some files were passed, otherwise default to the current working directory
-    let search_roots = if args.root.is_empty() {
-        let here = current_dir()?;
-        vec![here]
-    } else {
-        args.root
-    };
-
-    let mut file_handles = Vec::new();
-    for path in search_roots {
-        let span = tracing::debug_span!("", path = %path.display());
-        let _guard = span.enter();
-
-        tracing::debug!("listing files");
-
-        let files_tx = files_tx.clone();
-        file_handles.push(thread::spawn(move || {
-            if let Err(e) = find_test_files(&path, files_tx) {
-                tracing::warn!(error = %e, path = %path.display(), "finding test files");
+            if search_result.is_abort {
+                tracing::info!("no tests selected");
+                return Ok(());
             }
-        }));
-    }
-    drop(files_tx);
 
-    for handle in file_handles {
-        let _ = handle.join();
-    }
+            let selected_items = search_result.selected_items;
 
-    let files: Vec<_> = files_rx.into_iter().collect();
-    if files.is_empty() {
-        eyre::bail!("No compatible test files found");
-    }
-
-    tracing::debug!(n = files.len(), "finished collecting files");
-
-    let (test_tx, test_rx) = unbounded();
-    files
-        .into_par_iter()
-        .for_each_with(test_tx, |sender, path| {
-            if let Err(e) = parse_file(sender, &path) {
-                tracing::warn!(error = %e, path = %path.display(), "error parsing file");
+            if selected_items.is_empty() {
+                tracing::warn!("no tests selected");
+                return Ok(());
             }
-        });
 
-    if args.no_fizzy_selection {
-        for test in test_rx {
-            println!("{}", test.text());
+            if selected_items.len() > 1 {
+                panic!("programming error: multiple tests selected");
+            }
+
+            let test = selected_items[0].text();
+            state.set_last_test(test.clone())?;
+            println!("{test}");
+
+            Ok(())
         }
-
-        return Ok(());
+        Command::State { state_command } => match state_command {
+            StateCommand::Clear { all } => {
+                let cache_clear_option = if all {
+                    CacheClearOption::All
+                } else {
+                    CacheClearOption::Current
+                };
+                state
+                    .clear(cache_clear_option)
+                    .wrap_err("clearing cache state")?;
+                Ok(())
+            }
+        },
     }
-
-    // perform fuzzy search
-    let skim_options = SkimOptionsBuilder::default()
-        .multi(false)
-        .build()
-        .expect("invalid skim options");
-    let search_result = skim::Skim::run_with(&skim_options, Some(test_rx))
-        .ok_or_else(|| eyre::eyre!("performing interactive search"))?;
-
-    if search_result.is_abort {
-        tracing::info!("no tests selected");
-        return Ok(());
-    }
-
-    let selected_items = search_result.selected_items;
-
-    if selected_items.is_empty() {
-        tracing::warn!("no tests selected");
-        return Ok(());
-    }
-
-    if selected_items.len() > 1 {
-        panic!("programming error: multiple tests selected");
-    }
-
-    let test = selected_items[0].text();
-    state.set_last_test(test.clone())?;
-    println!("{test}");
-
-    Ok(())
 }
 
 struct Visitor<'s> {
