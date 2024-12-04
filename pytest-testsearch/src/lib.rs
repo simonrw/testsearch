98e11ca577954159eb932162a351d9ad4db0bb47
Simon Walker <s.r.walker101@googlemail.com>
2024-12-04 23:52:12 +0000

    Replace main collection function

--- pytest-testsearch/src/lib.rs
+++ pytest-testsearch/src/lib.rs
@@ -1,30 +1,20 @@
 use pyo3::{prelude::*, types::PyList};
 
-/// Collect pytest report
+/// Collect pytest items
 #[pyfunction]
-#[pyo3(signature = (collector))]
-fn pytest_make_collect_report<'py>(
+#[pyo3(signature = (session))]
+fn pytest_collection<'py>(
     py: Python<'py>,
-    collector: &Bound<'py, PyAny>,
-) -> PyResult<Bound<'py, PyAny>> {
-    todo!()
+    session: &Bound<'py, PyAny>,
+) -> PyResult<Bound<'py, PyList>> {
+    let _ = session;
+    let retval = PyList::empty(py);
+    Ok(retval)
 }
-
-// /// Collect pytest items
-// #[pyfunction]
-// #[pyo3(signature = (session))]
-// fn pytest_collection<'py>(
-//     py: Python<'py>,
-//     session: &Bound<'py, PyAny>,
-// ) -> PyResult<Bound<'py, PyList>> {
-//     let _ = session;
-//     let retval = PyList::empty(py);
-//     Ok(retval)
-// }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pytest_testsearch(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(pytest_make_collect_report, m)?)?;
+    m.add_function(wrap_pyfunction!(pytest_collection, m)?)?;
     Ok(())
 }
