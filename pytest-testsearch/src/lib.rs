1fa4dc3a9abad74a1b0b9cc52a780d4ee18939e8
Simon Walker <s.r.walker101@googlemail.com>
2024-12-04 23:41:21 +0000

    Set up pytest collect items plugin

--- pytest-testsearch/src/lib.rs
+++ pytest-testsearch/src/lib.rs
@@ -1,14 +1,30 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyList};
 
-/// Formats the sum of two numbers as string.
+/// Collect pytest report
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyo3(signature = (collector))]
+fn pytest_make_collect_report<'py>(
+    py: Python<'py>,
+    collector: &Bound<'py, PyAny>,
+) -> PyResult<Bound<'py, PyAny>> {
+    todo!()
 }
+
+// /// Collect pytest items
+// #[pyfunction]
+// #[pyo3(signature = (session))]
+// fn pytest_collection<'py>(
+//     py: Python<'py>,
+//     session: &Bound<'py, PyAny>,
+// ) -> PyResult<Bound<'py, PyList>> {
+//     let _ = session;
+//     let retval = PyList::empty(py);
+//     Ok(retval)
+// }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pytest_testsearch(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(pytest_make_collect_report, m)?)?;
     Ok(())
 }
